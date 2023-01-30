0bc286997c24ec84ddb775b2e6f4605f30e52ceb
VICTOR-CHUKWU <okenwa1993@gmail.com>
2023-01-30 20:52:15 +0100

    add interceptor

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,32 +3,6 @@ import { getToken } from "../utils";
 
 const token = getToken()
 
-axios.interceptors.response.use(
-    function (successRes) {
-        console.log(successRes, 'reso');
-
-        if (successRes.data.status === 401 && successRes.data.message === 'Please login to continue') {
-            localStorage.clear();
-            window.location.href = '/login';
-        }
-        return successRes;
-    },
-    function (error) {
-        console.log(error, 'poeror');
-
-        if (error.response?.status === 401) {
-            localStorage.clear()
-            window.location.href = '/login';
-            return Promise.reject(error);
-        }
-        if (error.request) {
-            return Promise.reject(error);
-        } else {
-            return Promise.reject(error);
-        }
-    }
-);
-
 const api = axios.create({
     baseURL: 'http://localhost:3001/api',
     timeout: 1000,
@@ -38,4 +12,11 @@ const api = axios.create({
     },
 });
 
+api.interceptors.response.use(data => data, (error) => {
+    if (error.response?.status === 401) {
+        localStorage.removeItem('todo-token');
+        window.location.href = '/login';
+    }
+    throw error;
+});
 export default api;
